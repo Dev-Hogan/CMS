16430b1c4a56d7799764d7ddcc376a5bc7d99e70
Hutao <mm_Huang_work@163.com>
2023-04-10 18:05:08 +0800

    添加响应拦截器

--- js/login.js
+++ js/login.js
@@ -2,6 +2,22 @@
 axios.defaults.baseURL = "http://www.itcbc.com:8000";
 //配置状态弹窗位置
 toastr.options.positionClass = "toast-top-center";
+// 添加响应拦截器
+axios.interceptors.response.use(function (response) {
+    // 2xx 范围内的状态码都会触发该函数。
+    if (response.data.code === 1) {
+        //提示用户失败
+        toastr.error(response.data.message)
+        return Promise.reject(new Error(response.data.message));
+    }
+    // 对响应数据做点什么
+    return response;
+  }, function (error) {
+    // 超出 2xx 范围的状态码都会触发该函数。
+    // 对响应错误做点什么
+    toastr.error(error.response.data.message)
+    return Promise.reject(error);
+  });
 
 //切换注册表单与登录表单
 //给两个按钮绑定切换事件
