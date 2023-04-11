05ba9761bd5443b22de1847498172dacf019a2e1
Hutao <mm_Huang_work@163.com>
2023-04-11 16:02:55 +0800

    下拉菜单动画实现方式二

--- js/index.js
+++ js/index.js
@@ -1,15 +1,11 @@
 const [li1,li2] = document.querySelectorAll(".menu-container li");
 li1.addEventListener("click", () => {
-    console.log(1);
     const drop = li1.querySelector('.child-menu')
-    console.log(drop);
-   drop.classList.toggle("open");
+   drop.style.height=drop.style.height===''?drop.scrollHeight+'px':''
 })
 li2.addEventListener("click", () => {
-    console.log(1);
     const drop = li2.querySelector('.child-menu')
-    console.log(drop);
-   drop.classList.toggle("open");
+   drop.style.height=drop.style.height===''?drop.scrollHeight+'px':''
 })
 
     
