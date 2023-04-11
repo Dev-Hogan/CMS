28da5dcfa27bf299d55543556d5844054c2f194c
Hutao <mm_Huang_work@163.com>
2023-04-11 16:14:40 +0800

    下拉动画方法二实现，样式实现（自己）

--- js/index.js
+++ js/index.js
@@ -1,11 +1,10 @@
-const [li1,li2] = document.querySelectorAll(".menu-container li");
+const [li1, li2] = document.querySelectorAll(".menu-container li");
 li1.addEventListener("click", () => {
-    const drop = li1.querySelector('.child-menu')
-   drop.style.height=drop.style.height===''?drop.scrollHeight+'px':''
-})
+    const drop = li1.querySelector(".child-menu");
+    drop.style.height = drop.style.height === "" ? drop.scrollHeight + "px" : "";
+    drop.style.backgroundColor = "#0d6efd";
+});
 li2.addEventListener("click", () => {
-    const drop = li2.querySelector('.child-menu')
-   drop.style.height=drop.style.height===''?drop.scrollHeight+'px':''
-})
-
-    
+    const drop = li2.querySelector(".child-menu");
+    drop.style.height = drop.style.height === "" ? drop.scrollHeight + "px" : "";
+});
