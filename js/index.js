e630e3ecd14cb68c7317b092432e11e0b69f2bce
Hutao <mm_Huang_work@163.com>
2023-04-11 16:29:06 +0800

    修复点击li就会全部收起的bug

--- js/index.js
+++ js/index.js
@@ -1,10 +1,17 @@
 const [li1, li2] = document.querySelectorAll(".menu-container li");
-li1.addEventListener("click", () => {
-    const drop = li1.querySelector(".child-menu");
-    drop.style.height = drop.style.height === "" ? drop.scrollHeight + "px" : "";
-    drop.style.backgroundColor = "#0d6efd";
+li1.addEventListener("click", (e) => {
+    if (e.target.classList.contains("level1-menu")) {
+        const drop = li1.querySelector(".child-menu");
+        drop.style.height =
+        drop.style.height === "" ? drop.scrollHeight + "px" : "";
+        drop.style.backgroundColor = "#0d6efd";
+    }
 });
-li2.addEventListener("click", () => {
-    const drop = li2.querySelector(".child-menu");
-    drop.style.height = drop.style.height === "" ? drop.scrollHeight + "px" : "";
+li2.addEventListener("click", (e) => {
+    if (e.target.classList.contains("level1-menu")) {
+        const drop = li2.querySelector(".child-menu");
+        drop.style.height =
+        drop.style.height === "" ? drop.scrollHeight + "px" : "";
+    }
 });
+
