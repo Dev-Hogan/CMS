928d56ff9f20f9e57b88c8bf15e0cc5a9dda46b6
Hutao <mm_Huang_work@163.com>
2023-04-11 16:51:50 +0800

    老师的方法

--- js/index.js
+++ js/index.js
@@ -1,17 +1,10 @@
-const [li1, li2] = document.querySelectorAll(".menu-container li");
-li1.addEventListener("click", (e) => {
-    if (e.target.classList.contains("level1-menu")) {
-        const drop = li1.querySelector(".child-menu");
-        drop.style.height =
-        drop.style.height === "" ? drop.scrollHeight + "px" : "";
-        drop.style.backgroundColor = "#0d6efd";
-    }
-});
-li2.addEventListener("click", (e) => {
-    if (e.target.classList.contains("level1-menu")) {
-        const drop = li2.querySelector(".child-menu");
-        drop.style.height =
-        drop.style.height === "" ? drop.scrollHeight + "px" : "";
-    }
-});
+//一级菜单绑定点击，展开二级菜单
+const level1Menus = document.querySelectorAll(".level-1-menu");
 
+[...level1Menus].forEach((menu) => {
+  menu.addEventListener("click", () => {
+    const subMenu = menu.nextElementSibling;
+    subMenu.style.height =
+      subMenu.style.height === "" ? subMenu.scrollHeight + "px" : "";
+  });
+});
