f2edf4f14a268b4bfc5bc688e1b6c6827770a579
Hutao <mm_Huang_work@163.com>
2023-04-13 15:47:43 +0800

    完成首页嵌套基本表格

---
+++ js/common.js
@@ -0,0 +1,25 @@
+// 配置axios公共基地址
+axios.defaults.baseURL = "http://www.itcbc.com:8000";
+// 添加响应拦截器
+axios.interceptors.response.use(
+  function (response) {
+    // 2xx 范围内的状态码都会触发该函数。
+    if (response.data.code === 1) {
+      // 接口是失败的
+      // 提示用户
+      toastr.error(response.data.message);
+      // 你可以返回一个失败的Promise，阻止接口正常返回
+      // Promise.reject()表示创建一个失败的Promise
+      return Promise.reject(new Error(response.data.message));
+    }
+    // 对响应数据做点什么
+    return response;
+  },
+  function (error) {
+    // 提示用户错误消息
+    toastr.error(error.response.data.message);
+    // 超出 2xx 范围的状态码都会触发该函数。
+    // 对响应错误做点什么
+    return Promise.reject(error);
+  }
+);
