3acc2f3752536b2cf85f068dbf44a2193ecbe9b3
Hutao <mm_Huang_work@163.com>
2023-04-14 12:13:58 +0800

    完成学员录入

--- js/score.js
+++ js/score.js
@@ -9,10 +9,45 @@ async function render () {
       html += `<tr>
                 <th scope="row">${id}</th>
                 <td>${student.name}</td>
-                ${student.score.map((score) => `<td class="student-score" >${score===null?'-':score}</td>`).join("")}
+                ${student.score
+                  .map(
+                    (score, index) =>
+                      `<td class="student-score" data-id="${id}" data-batch="${index+1}" >${
+                        score === null ? "-" : score
+                      }</td>`
+                  )
+                  .join("")}
             </tr>`;
     }
     tbody.innerHTML = html;
 }
 //初次渲染
 render()
+
+//双击显示可以修改分数
+tbody.addEventListener('dblclick', e => {
+    const target = e.target
+    if (target.classList.contains('student-score')) {
+        const input = document.createElement('input')
+        input.value = target.textContent
+        target.append(input)
+        input.focus()
+
+        //失去焦点
+        input.onblur = () => input.remove()
+        input.onkeyup = async (e) => {
+            const td = input.parentElement
+            console.log(td.dataset.id);
+            if (e.key === 'Enter') {
+                const res = await axios.post("/score/entry", {
+                    stu_id: td.dataset.id,
+                    batch: td.dataset.batch,
+                    score: +input.value
+                })
+                console.log(res);
+                toastr.success(res.data.message)
+                render()
+            }
+        }
+    }
+})
